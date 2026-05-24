6656acb8e38073c067412a374c750deac88bb1f7
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-24 21:11:42 +0600

    feat(filterEvenNumbers): implement function to return even numbers from array

---
+++ dist/solutions.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Problem-1
+function filterEvenNumbers(arr) {
+    return arr.filter(num => num % 2 == 0);
+}
+filterEvenNumbers([1, 2, 3, 4, 5, 6]);
+//# sourceMappingURL=solutions.js.map
---
+++ src/solutions.ts
@@ -0,0 +1,5 @@
+// Problem-1
+function filterEvenNumbers(arr:number[]) : number[]{
+    return arr.filter(num => num % 2 ==0)
+}
+// console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
