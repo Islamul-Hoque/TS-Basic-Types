89c2a0200b8042617529a705ad38d754596b4811
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-24 21:12:28 +0600

    feat(reverseString): add function to reverse a string using split, reverse, and join

--- src/solutions.ts
+++ src/solutions.ts
@@ -3,3 +3,9 @@ function filterEvenNumbers(arr:number[]) : number[]{
     return arr.filter(num => num % 2 ==0)
 }
 // console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+// Problem-2
+function reverseString(arr: string): string{
+    return arr.split('').reverse().join('');
+}
+// console.log(reverseString("typescript"));
